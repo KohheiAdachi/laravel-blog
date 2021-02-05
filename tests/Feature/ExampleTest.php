cb19a3778804625a98a1388fa75681c8d5de442d
KohheiAdachi <kohei.adachi805@mail.kyutech.jp>
2021-02-05 16:32:47 +0900

    add

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Profile;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -14,6 +15,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
+        Profile::factory()->create();
         $response = $this->get('/');
 
         $response->assertStatus(200);
